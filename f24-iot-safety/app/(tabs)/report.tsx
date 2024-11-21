59e7cd60b1578eb643f35e6316242cf24b153210
Saahir Narang <saahir.narang@gmail.com>
2024-11-20 19:29:54 -0600

    some dependy changes

--- f24-iot-safety/app/(tabs)/report.tsx
+++ f24-iot-safety/app/(tabs)/report.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
 import { View, Text, ScrollView, StyleSheet } from 'react-native';
 
@@ -9,41 +8,10 @@ import { View, Text, ScrollView, StyleSheet } from 'react-native';
 // import PlayerReport from './screens/PlayerReport'; // Import the new Player Report screen
 
 
-const Tab = createBottomTabNavigator();
+// const Tab = createBottomTabNavigator();
 
 
-const App: React.FC = () => {
- return (
-   <NavigationContainer>
-     <Tab.Navigator
-       screenOptions={({ route }) => ({
-         headerShown: false,
-         tabBarIcon: ({ focused, color, size }) => {
-           let iconName;
+const Report: React.FC = () => {};
 
 
-           if (route.name === 'Roster Report') {
-             iconName = focused ? 'list-circle' : 'list-circle-outline';
-           } else if (route.name === 'New Page') {
-             iconName = focused ? 'add-circle' : 'add-circle-outline';
-           } else if (route.name === 'Player Report') {
-             iconName = focused ? 'person' : 'person-outline';
-           }
-
-
-           return <Ionicons name={iconName as any} size={size} color={color} />;
-         },
-         tabBarActiveTintColor: 'tomato',
-         tabBarInactiveTintColor: 'gray',
-       })}
-     >
-       {/* <Tab.Screen name="Roster Report" component={BlitzRosterReport} />
-       <Tab.Screen name="New Page" component={NewPage} />
-       <Tab.Screen name="Player Report" component={PlayerReport} /> */}
-     </Tab.Navigator>
-   </NavigationContainer>
- );
-};
-
-
-export default App;
+export default Report;
